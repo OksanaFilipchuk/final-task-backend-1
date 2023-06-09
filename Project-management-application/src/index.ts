2d65860d783ee0b086353fb8489c7a9a5aa078a2
Oksana Filipchuk <oksanafilipchuk@MacBook-Air-Oksana.local>
2023-06-10 00:01:50 +0300

    add user info

--- Project-management-application/src/index.ts
+++ Project-management-application/src/index.ts
@@ -4,9 +4,11 @@ import { PORT } from './constants';
 import * as serverService from './services/server.service';
 
 
+
+
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb://oksanafilipchuk:StOJ9LGlgX97Aujs@ac-wiefxtm-shard-00-00.it0zwnt.mongodb.net:27017,ac-wiefxtm-shard-00-01.it0zwnt.mongodb.net:27017,ac-wiefxtm-shard-00-02.it0zwnt.mongodb.net:27017/?ssl=true&replicaSet=atlas-10y87v-shard-0&authSource=admin&retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
